366cf679f890937284b8e54eea73d46a4ad3b4c4
Liz Emerson <uknity@gmail.com>
2021-05-07 13:36:56 -0400

    added models; added morgan to server.js; modified initial routes/api.js

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Please enter they type of exercise you'll be doing.",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Please enter an exercise name.",
+      },
+      duration: {
+        type: Number,
+        required: "Please enter the duration of your exercise in minutes.",
+      },
+      weight: {
+        type: Number,
+      }
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
