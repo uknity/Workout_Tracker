5bf3fd6a058124dc89c763ef768b38eec81eaf92
Liz Emerson <uknity@gmail.com>
2021-05-11 11:49:52 -0400

    configured port information to work with Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 // const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -12,9 +12,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout"
-mongoose.connect("mongodb://localhost/workout"
-, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
